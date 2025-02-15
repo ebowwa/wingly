74c397c296a3ce7f0aa134f368bea5846235b4c8
e r <37605096-caringmindinfo@users.noreply.replit.com>
2025-02-15 23:53:40 +0000

    Assistant checkpoint: Fix foreign key references in Friendship model
    Assistant generated file changes:
    - models/friendship.py: Fix foreign key references in Friendship model
    
    ---
    
    User prompt:
    
    still unsolved

--- models/friendship.py
+++ models/friendship.py
@@ -3,8 +3,8 @@ from . import db
 
 class Friendship(db.Model):
     __tablename__ = 'friendships'
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
-    friend_id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), primary_key=True)
+    friend_id = db.Column(db.Integer, db.ForeignKey('users.id'), primary_key=True)
     became_friends_at = db.Column(db.DateTime, default=datetime.utcnow)
     last_interaction = db.Column(db.DateTime, default=datetime.utcnow)
     interaction_streak = db.Column(db.Integer, default=0)
